1e10b3743515f797e30baab9f0477f717e242091
anais arce <anaisarce89@gmail.com>
2024-04-25 11:31:21 -0500

    updating again

--- 01.py
+++ 01.py
@@ -6,3 +6,10 @@ print(music_data.head())
 
 music_data.info()
 print(music_data.describe())
+music_data.isnull().sum()
+# dropping the 'Unnamed: 0' column
+music_data_cleaned = music_data.drop(columns=['Unnamed: 0'])
+
+# handling missing values by filling them with placeholder text
+columns_with_missing_values = ['Track Name', 'Artists', 'Album Name']
+music_data_cleaned[columns_with_missing_values] = music_data_cleaned[columns_with_missing_values].fillna('Unknown')
