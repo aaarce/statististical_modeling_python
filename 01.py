963d0bac6deac69c2b949f993f20e1bffb4a6eb4
anais arce <anaisarce89@gmail.com>
2024-04-25 11:43:47 -0500

    plotting the distribution of popularity

--- 01.py
+++ 01.py
@@ -13,3 +13,15 @@ music_data_cleaned = music_data.drop(columns=['Unnamed: 0'])
 # handling missing values by filling them with placeholder text
 columns_with_missing_values = ['Track Name', 'Artists', 'Album Name']
 music_data_cleaned[columns_with_missing_values] = music_data_cleaned[columns_with_missing_values].fillna('Unknown')
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="whitegrid")
+
+# plotting the distribution of popularity
+plt.figure(figsize=(10, 6))
+sns.histplot(music_data_cleaned['Popularity'], bins=20, kde=True)
+plt.title('Distribution of Popularity Scores')
+plt.xlabel('Popularity Score')
+plt.ylabel('Frequency')
+plt.show()
